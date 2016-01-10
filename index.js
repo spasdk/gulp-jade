801e5e6dec43eae3a661ab2c60824db863ce97e1
DarkPark <darkpark@pisem.net>
2016-01-10 23:32:40 +0200

    generate full set of tasks with group aliases

--- index.js
+++ index.js
@@ -14,29 +14,31 @@ var fs       = require('fs'),
     jade     = require('jade'),
     del      = require('del'),
     tools    = require('spa-gulp/tools'),
-    config   = tools.load(path.join(process.env.PATH_ROOT, process.env.PATH_CFG, 'jade'), path.join(__dirname, 'config')),
+    gulpName = 'jade',
+    config   = tools.load(path.join(process.env.PATH_ROOT, process.env.PATH_CFG, gulpName), path.join(__dirname, 'config')),
     pkgInfo  = require(process.env.PACKAGE),
     outFiles = [],
-    taskList = [];
+    buildTasks = [],
+    cleanTasks = [];
 
 
-function compile ( config, done ) {
-    var srcFile = path.join(process.env.PATH_ROOT, process.env.PATH_SRC, config.srcPath, config.srcFile),
-        outFile = path.join(process.env.PATH_ROOT, process.env.PATH_APP, config.outPath, config.outFile),
+function compile ( profile, done ) {
+    var sourceFile = path.join(process.env.PATH_ROOT, process.env.PATH_SRC, profile.sourcePath, profile.sourceFile),
+        targetFile = path.join(process.env.PATH_ROOT, process.env.PATH_APP, profile.targetPath, profile.targetFile),
         render  = null;
 
     try {
         // prepare function
-        render = jade.compileFile(srcFile, {
-            filename: srcFile,
-            pretty: config.indentString
+        render = jade.compileFile(sourceFile, {
+            filename: sourceFile,
+            pretty: profile.indentString
         });
 
         // save generated result
-        fs.writeFileSync(outFile, render(config.variables));
+        fs.writeFileSync(targetFile, render(profile.variables));
     } catch ( error ) {
         // console log + notification popup
-        tools.error('jade', error.message);
+        tools.error(gulpName, error.message);
     }
 
     done();
@@ -49,14 +51,34 @@ if ( !config.active ) {
 }
 
 
+// build global task tree
+//global.tasks.build[gulpName] = [];
+//global.tasks.watch[gulpName] = [];
+//global.tasks.clean[gulpName] = [];
+//global.tasks[gulpName] = {
+//    build: [],
+//    watch: [],
+//    clean: []
+//};
+global.tasks[gulpName] = [gulpName + ':build'];
+global.tasks[gulpName + ':build'] = [];
+global.tasks[gulpName + ':watch'] = [];
+global.tasks[gulpName + ':clean'] = [];
+global.tasks.build.push(gulpName + ':build');
+global.tasks.watch.push(gulpName + ':watch');
+global.tasks.clean.push(gulpName + ':clean');
+
+
 // only derived profiles are necessary
 delete config.profiles.default;
 
 
 // generate tasks by config profiles
 Object.keys(config.profiles).forEach(function ( profileName ) {
-    var profile  = config.profiles[profileName],
-        taskName = 'jade:' + profileName;
+    var profile   = config.profiles[profileName],
+        buildName = gulpName + ':build:' + profileName,
+        watchName = gulpName + ':watch:' + profileName,
+        cleanName = gulpName + ':clean:' + profileName;
 
     // sanitize
     profile.variables = profile.variables || {};
@@ -68,47 +90,76 @@ Object.keys(config.profiles).forEach(function ( profileName ) {
     profile.variables.author      = profile.variables.author      || pkgInfo.author;
     profile.variables.license     = profile.variables.license     || pkgInfo.license;
 
-    // protect from upper content deletion
-    if ( profile.outFile ) {
-        // files to delete in clear task
-        outFiles.push(path.join(process.env.PATH_APP, profile.outPath || '', profile.outFile));
-    }
-
     // profile build task
-    gulp.task(taskName, function ( done ) {
+    gulp.task(buildName, function ( done ) {
         compile(profile, done);
+    });
+
+    // remove all generated html files
+    gulp.task(cleanName, function () {
+        var files = [];
+
+        // protect from upper content deletion
+        if ( profile.targetFile ) {
+            // files to delete in clear task
+            files.push(path.join(process.env.PATH_APP, profile.targetPath || '', profile.targetFile));
+        }
+
+        tools.log('clean', del.sync(files));
     });
 
     // profile watch task
     if ( profile.watch ) {
         // done callback should be present to show gulp that task is not over
-        gulp.task(taskName + ':watch', function ( done ) {
+        gulp.task(watchName, function ( done ) {
             gulp.watch([
                 //process.env.PACKAGE,
-                path.join(process.env.PATH_SRC, profile.srcPath, '**', '*.jade')
-            ], [taskName]);
+                path.join(process.env.PATH_SRC, profile.sourcePath, '**', '*.' + gulpName)
+            ], [buildName]);
         });
+
+        // register global tasks
+        //global.tasks.watch[gulpName].push(watchName);
+        //global.tasks[gulpName].watch.push(watchName);
+        global.tasks[gulpName + ':watch'].push(watchName);
     }
 
-    // fill group task list
-    taskList.push(taskName);
+    // register global tasks
+    //global.tasks.build[gulpName].push(buildName);
+    //global.tasks.clean[gulpName].push(cleanName);
+    //global.tasks[gulpName].build.push(buildName);
+    //global.tasks[gulpName].clean.push(cleanName);
+    global.tasks[gulpName + ':build'].push(buildName);
+    global.tasks[gulpName + ':clean'].push(cleanName);
+
+    global.tasks['build:' + profileName] = global.tasks['build:' + profileName] || [];
+    global.tasks['build:' + profileName].push(buildName);
+
+    global.tasks['clean:' + profileName] = global.tasks['clean:' + profileName] || [];
+    global.tasks['clean:' + profileName].push(cleanName);
 });
 
 
 // output current config
-gulp.task('jade:config', function () {
-    tools.log('jade', util.inspect(config, {depth: 5, colors: true}));
+gulp.task(gulpName + ':config', function () {
+    tools.log(gulpName, util.inspect(config, {depth: 5, colors: true}));
 });
 
 
-// remove all generated html files
-gulp.task('jade:clean', function () {
-    del.sync(outFiles);
-});
+//// remove all generated html files
+//gulp.task(gulpName + ':clean', function () {
+//    tools.log('clean', del.sync(outFiles));
+//});
+
+// register in global task "clean"
+//global.tasks.clean.push('jade:clean');
+//global.tasks.build.push('jade:build');
 
 
 // run all profiles tasks
-gulp.task('jade', taskList);
+//gulp.task('jade:build', buildTasks);
+//gulp.task('jade:clean', cleanTasks);
+//gulp.task('jade', ['jade:build']);
 
 
 // public
