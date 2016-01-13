ae3f4a367cb398a498e5cafc79d388b9b3e4d880
Stanislav Kalashnik <sk@infomir.eu>
2016-01-13 18:45:41 +0200

    rework config

--- config.js
+++ config.js
@@ -7,49 +7,25 @@
 
 'use strict';
 
+var path = require('path');
+
 // set of named configs for corresponding gulp tasks
 // each profile inherits all options from the "default" profile
 module.exports = {
-    // config to be extended by other profiles
     default: {
         // directory to look for source files
-        // default: <project root>/src/jade
-        sourcePath: 'jade',
+        sourcePath: path.join(process.env.PATH_SRC, 'jade'),
 
         // main source entry point
         sourceFile: 'main.jade',
 
         // directory to store output files
-        // default: <project root>/app/
-        targetPath: ''
-    },
+        targetPath: process.env.PATH_APP,
 
-    // config for jade:build:develop task
-    develop: {
-        // intended output file name
-        targetFile: 'develop.html',
-
-        // indentation to use in the output file
-        // use some string or false to disable
-        indentString: '    ',
-
-        // local variables available in the jade source files
-        // built-in vars: name, version, description, author, license
-        variables: {
-            develop: true
-        },
-
-        // create task jade:watch:develop
-        // to automatically rebuild on source files change
-        watch: true
-    },
-
-    // config for jade:build:release task
-    release: {
         // intended output file name
         targetFile: 'index.html',
 
-        // indentation to use in the output file
+        // indentation to use in the target file
         // use some string or false to disable
         indentString: false,
 
@@ -57,6 +33,22 @@ module.exports = {
         // built-in vars: name, version, description, author, license
         variables: {
             develop: false
-        }
+        },
+
+        // create watch task
+        // to automatically rebuild on source files change
+        watch: false
+    },
+
+    develop: {
+        targetFile: 'develop.html',
+
+        indentString: '    ',
+
+        variables: {
+            develop: true
+        },
+
+        watch: true
     }
 };
